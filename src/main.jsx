fb12161a2a920ae6de574651a68f464abb9839f3
Kirill Romanov <58993148+qwerty123t@users.noreply.github.com>
2023-12-23 21:00:58 +0300

    Добавил сброс стилей

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './assets/reset.css'
+import './assets/app.css'
+import './assets/fonts.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
